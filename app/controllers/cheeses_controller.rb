f1344b049291158cbf36304057de5ef8cc33592a
leilaotieno2 <leila.otieno@student.moringaschool.com>
2023-08-23 17:22:44 +0300

    first commit

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -6,10 +6,13 @@ class CheesesController < ApplicationController
     render json: cheeses
   end
 
-  # GET /cheeses/:id
   def show
     cheese = Cheese.find_by(id: params[:id])
-    render json: cheese
+    if cheese
+      render json: cheese, except: [:created_at, :updated_at], methods: [:summary]
+    else
+      # status: :not_found will produce a 404 status code
+      render json: { error: 'Cheese not found' }, status: :not_found
+    end
   end
-
 end
